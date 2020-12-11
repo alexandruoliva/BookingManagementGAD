1247f9eaf773527e95edcb8b4b4482a52c771b65
unknown <olivaalexandru@yahoo.com>
2020-12-12 01:03:55 +0200

    Created 'Login' and 'Register' pages

--- app/src/main/java/com/example/bookingmanagementgad/Register.java
+++ app/src/main/java/com/example/bookingmanagementgad/Register.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
