9f9a0552ab7c7fcbf95e07b6952d0c12de82c6b8
Oliva Alexandru <olivaalexandru@yahoo.com>
2020-12-23 23:05:20 +0200

    Added graphical elements in the Register class

--- app/src/main/java/com/example/bookingmanagementgad/Register.java
+++ app/src/main/java/com/example/bookingmanagementgad/Register.java
@@ -1,14 +1,42 @@
 package com.example.bookingmanagementgad;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    EditText mFirstName, mLastName, mEmail, mPassword, mConfirmPassword;
+    Button mRegisterButton;
+    TextView mLoginButton;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirstName = findViewById(R.id.editTextFirstName);
+        mLastName = findViewById(R.id.editTextLastName);
+        mEmail = findViewById(R.id.editTextTextEmailAddressRegister);
+        mPassword = findViewById(R.id.editTextPasswordRegister);
+        mConfirmPassword = findViewById(R.id.editTextPasswordConfirmRegister);
+
+        mRegisterButton =findViewById(R.id.registerBtn);
+        mLoginButton = findViewById(R.id.goToLoginPage);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+
+
+
+
     }
 }
