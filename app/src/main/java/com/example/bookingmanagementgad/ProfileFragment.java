884e5f5599c7fb0eafc185377234554c9d65a3a7
alexandruoliva <olivaalexandru@yahoo.com>
2021-01-20 22:06:39 +0200

    Implemented the feature to update the user's profile

--- app/src/main/java/com/example/bookingmanagementgad/ProfileFragment.java
+++ app/src/main/java/com/example/bookingmanagementgad/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.bookingmanagementgad;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,9 +27,13 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "ProfileFragment";
 
     @Nullable
     @Override
@@ -46,23 +51,55 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         TextView mVerifyMessage = (TextView) view.findViewById(R.id.emailNotVerifiedTextView);
         Button mVerifyButton = (Button) view.findViewById(R.id.verifyEmailButton);
-        EditText mFirstNameTextView = (EditText) view.findViewById(R.id.editTextDashboardFirstName);
-        EditText mLastNameTextView = (EditText) view.findViewById(R.id.editTextDashboardLastName);
+        Button mUpdateUser = (Button) view.findViewById(R.id.buttonUpdateProfileFragment);
+        EditText mFirstNameTextEdit = (EditText) view.findViewById(R.id.editTextDashboardFirstName);
+        EditText mLastNameTextEdit = (EditText) view.findViewById(R.id.editTextDashboardLastName);
         TextView mEmailAddressTextView = (TextView) view.findViewById(R.id.textViewDashboardEmail);
 
-        fetchUserData(documentReference, mFirstNameTextView, mLastNameTextView, mEmailAddressTextView);
-
+        fetchUserData(documentReference, mFirstNameTextEdit, mLastNameTextEdit, mEmailAddressTextView);
+        updateUserProfile(fAuth, documentReference, mUpdateUser, mFirstNameTextEdit, mLastNameTextEdit, mEmailAddressTextView);
         displayGraphicaElementsIfEmailNotVerified(fUser, mVerifyMessage, mVerifyButton);
 
         return view;
     }
 
-    private void fetchUserData(DocumentReference documentReference, TextView mFirstNameTextView, TextView mLastNameTextView, TextView mEmailAddressTextView) {
+    private void updateUserProfile(FirebaseAuth fAuth, DocumentReference documentReference, Button mUpdateUser, EditText mFirstNameTextEdit, EditText mLastNameTextEdit, TextView mEmailAddressTextView) {
+        mUpdateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userID = fAuth.getCurrentUser().getUid();
+                String firstName = mFirstNameTextEdit.getText().toString();
+                String lastName = mLastNameTextEdit.getText().toString();
+                String email = mEmailAddressTextView.getText().toString();
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("fName", firstName);
+                user.put("lName", lastName);
+                user.put("email", email);
+                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSucces: user Profile is updated for " + userID);
+                        Toast.makeText(v.getContext(), "Profile has been updated successfully.", Toast.LENGTH_SHORT);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: " + e.toString());
+                        Toast.makeText(v.getContext(), "Fail! The profile has not been updated successfully.", Toast.LENGTH_SHORT);
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void fetchUserData(DocumentReference documentReference, EditText mFirstNameTextEdit, EditText mLastNameTextEdit, TextView mEmailAddressTextView) {
         documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-                mFirstNameTextView.setText(documentSnapshot.getString("fName"));
-                mLastNameTextView.setText(documentSnapshot.getString("lName"));
+                mFirstNameTextEdit.setText(documentSnapshot.getString("fName"));
+                mLastNameTextEdit.setText(documentSnapshot.getString("lName"));
                 mEmailAddressTextView.setText(documentSnapshot.getString("email"));
             }
         });
@@ -91,5 +128,6 @@ public class ProfileFragment extends Fragment {
             });
         }
     }
+
 
 }
