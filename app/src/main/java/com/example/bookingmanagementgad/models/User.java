fda31fcb1e21556821a497a934b6c4f86e7f0f0a
alexandruoliva <olivaalexandru@yahoo.com>
2021-01-21 23:39:21 +0200

    Created the model class 'User'

---
+++ app/src/main/java/com/example/bookingmanagementgad/models/User.java
@@ -0,0 +1,39 @@
+package com.example.bookingmanagementgad.models;
+
+public class User {
+
+    private String firstName, lastName, email;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
