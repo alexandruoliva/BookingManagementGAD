2e33eb434172530c8bd79c18766aaa3f47322859
alexandruoliva <olivaalexandru@yahoo.com>
2021-01-27 00:50:19 +0200

    Created the 'Add new booking' fragment

---
+++ app/src/main/java/com/example/bookingmanagementgad/GUI/NewBookingFragment.java
@@ -0,0 +1,27 @@
+package com.example.bookingmanagementgad.GUI;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bookingmanagementgad.R;
+
+public class NewBookingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+
+
+    }
+
+}
