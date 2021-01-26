379f6f7f17368be242ab24012477d9909ac67341
alexandruoliva <olivaalexandru@yahoo.com>
2021-01-26 22:13:23 +0200

    Added the personalised 'welcome back' title in the dashboard activity

--- app/src/main/java/com/example/bookingmanagementgad/DashboardActivityCardView.java
+++ app/src/main/java/com/example/bookingmanagementgad/DashboardActivityCardView.java
@@ -1,17 +1,31 @@
 package com.example.bookingmanagementgad;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.bookingmanagementgad.models.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class DashboardActivityCardView extends AppCompatActivity {
 
     private Button logOutButton;
+    private TextView titleDashboard;
+    private FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    private FirebaseUser fUser = fAuth.getCurrentUser();
+    private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    private String userId = fAuth.getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,18 @@ public class DashboardActivityCardView extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard_card_view);
 
         logOutButton = findViewById(R.id.logoutButtonDashboard);
+        titleDashboard = findViewById(R.id.userTitleTextViewDashboard);
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                User user = documentSnapshot.toObject(User.class);
+                titleDashboard.setText("Welcome back "+ user.getFirstName().toString()+" !");
+            }
+        });
+
         logOutButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
