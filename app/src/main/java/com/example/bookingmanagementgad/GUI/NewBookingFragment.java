512895352034e3451c64a3007e011bb48e5f7460
alexandruoliva <olivaalexandru@yahoo.com>
2021-01-28 22:27:17 +0200

    Refactored the id's for the Booking page GUI elements

--- app/src/main/java/com/example/bookingmanagementgad/GUI/NewBookingFragment.java
+++ app/src/main/java/com/example/bookingmanagementgad/GUI/NewBookingFragment.java
@@ -18,9 +18,7 @@ public class NewBookingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_booking, container, false);
-
         return view;
-
 
     }
 
