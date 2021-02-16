0ec7968c2f74bdad7bc2e1661cd4cd19e6bc7f5a
alexandruoliva <olivaalexandru@yahoo.com>
2021-02-16 23:54:58 +0200

    Implemented the method to uncheck other checboxes when one room is checked

--- app/src/main/java/com/example/bookingmanagementgad/GUI/FRAGMENTS/NewBookingFragment.java
+++ app/src/main/java/com/example/bookingmanagementgad/GUI/FRAGMENTS/NewBookingFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -31,12 +32,15 @@ public class NewBookingFragment extends Fragment {
     private EditText mEditTextCheckInDateBooking, mEditTextCheckOutDateBooking;
     private EditText mEditTypeOfBooking;
 
+    private ArrayList<CheckBox> checkBoxes = new ArrayList<>();
     private CheckBox mCheckBoxRoom1,mCheckBoxRoom2,mCheckBoxRoom3;
     private CheckBox mCheckBoxRoom4,mCheckBoxRoom5,mCheckBoxRoom6;
     private CheckBox mCheckBoxRoomUnderHouse,mCheckBoxRoomAll;
 
     private FirebaseAuth fAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+
+
 
     @Nullable
     @Override
@@ -51,19 +55,52 @@ public class NewBookingFragment extends Fragment {
         mEditTextCheckInDateBooking = view.findViewById(R.id.editTextCheckInDateBooking);
         mEditTextCheckOutDateBooking = view.findViewById(R.id.editTextCheckOutDateBooking);
         mEditTypeOfBooking = view.findViewById(R.id.editTextTypeOfBookingBooking);
+
         mCheckBoxRoom1 = view.findViewById(R.id.room1CheckBox);
+        checkBoxes.add(mCheckBoxRoom1);
         mCheckBoxRoom2 = view.findViewById(R.id.room2CheckBox);
+        checkBoxes.add(mCheckBoxRoom2);
         mCheckBoxRoom3 = view.findViewById(R.id.room3CheckBox);
+        checkBoxes.add(mCheckBoxRoom3);
         mCheckBoxRoom4 = view.findViewById(R.id.room4CheckBox);
+        checkBoxes.add(mCheckBoxRoom4);
         mCheckBoxRoom5 = view.findViewById(R.id.room5CheckBox);
+        checkBoxes.add(mCheckBoxRoom5);
         mCheckBoxRoom6 = view.findViewById(R.id.room6CheckBox);
+        checkBoxes.add(mCheckBoxRoom6);
         mCheckBoxRoomUnderHouse = view.findViewById(R.id.roomUnderHouseCheckBox);
+        checkBoxes.add(mCheckBoxRoomUnderHouse);
         mCheckBoxRoomAll = view.findViewById(R.id.roomAllCheckBox);
 
+        disableOtherCheckboxes();
         createNewBooking();
         return view;
 
     }
+
+    private void disableOtherCheckboxes(){
+        mCheckBoxRoomAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
+                if (mCheckBoxRoomAll.isChecked()){
+                    for(CheckBox checkBox : checkBoxes)
+                    {
+                        checkBox.setEnabled(false);
+                        if(checkBox.isChecked()){
+                            checkBox.toggle();
+                        }
+                    }
+                }
+                if (!mCheckBoxRoomAll.isChecked()){
+                    for(CheckBox checkBox : checkBoxes)
+                    {
+                        checkBox.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+
 
     private void createNewBooking() {
         mButtonCreateNewBooking.setOnClickListener(new View.OnClickListener() {
